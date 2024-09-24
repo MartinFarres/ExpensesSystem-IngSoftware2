780c1dc6c674c4262f10e8f99bb2f24749491a34
paulisuden <spaulinaabril@gmail.com>
2024-09-24 18:15:44 -0300

    metodo listar direccion activa

--- Interisys/Interisys-ejb/src/java/com/interisys/business/persistence/DAODireccionBean.java
+++ Interisys/Interisys-ejb/src/java/com/interisys/business/persistence/DAODireccionBean.java
@@ -2,6 +2,7 @@ package com.interisys.business.persistence;
 
 import com.interisys.business.domain.entity.Consorcio;
 import com.interisys.business.domain.entity.Direccion;
+import java.util.Collection;
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
@@ -51,6 +52,38 @@ public class DAODireccionBean {
             ex.printStackTrace();
             throw new ErrorDAOException("Error de sistema");
      } 
+   }
+   
+   public Collection<Direccion> listarDireccionActiva() throws ErrorDAOException{
+       
+       try{
+       
+        return em.createQuery("SELECT d "
+                           + "  FROM Direccion d"
+                           + " WHERE d.eliminado = FALSE").
+                           getResultList();
+       
+      } catch (Exception ex) {
+            ex.printStackTrace();
+            throw new ErrorDAOException("Error del sistema");
+      }
+   }
+   
+   public Collection<Direccion> listarDireccionActiva(String idLocalidad)throws ErrorDAOException{
+      
+      try{ 
+          
+       return em.createQuery("SELECT p "
+                           + "  FROM Direccion p"
+                           + " WHERE p.eliminado = FALSE"
+                           + "   AND p.localidad.id = :idLocalidad").
+                           setParameter("idLocalidad", idLocalidad).
+                           getResultList();
+       
+      } catch (Exception ex) {
+            ex.printStackTrace();
+            throw new ErrorDAOException("Error del sistema");
+      } 
    }
     
 }
